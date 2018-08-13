3f289ab193056190977880acdc866cd04e507a1b
Hannah Carnes <32318292+hcarnes@users.noreply.github.com>
2018-08-12 20:51:47 -0400

    blogs render in markdown (#4)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_markdown(markdown_string)
+    renderer = Redcarpet::Render::Safe.new(tables: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer, autolink: true)
+    markdown.render(markdown_string).html_safe
+  end
 end
